3601af3d8e37cd4d7353b7e3a52433743179175d
mguida22 <michael.guida@colorado.edu>
2016-04-06 01:27:46 -0600

    layout, support multiple candidates

--- website/index.js
+++ website/index.js
@@ -1,12 +1,14 @@
+function random() {
+  return Math.floor(Math.random() * 101);
+}
+
 var n = 50,
   duration = 750,
-  now = new Date(Date.now() - duration),
-  random = function() { return Math.floor(Math.random() * 101); },
-  data = d3.range(n).map(random);
+  now = new Date(Date.now() - duration);
 
 var margin = {top: 20, right: 20, bottom: 20, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 400 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
 
 var x = d3.time.scale()
     .domain([now - (n - 2) * duration, now - duration])
@@ -21,61 +23,72 @@ var line = d3.svg.line()
     .x(function(d, i) { return x(now - (n - 1 - i) * duration); })
     .y(function(d, i) { return y(d); });
 
-var svg = d3.select(".graph").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+function graph(id) {
+  var svg = d3.select("#" + id + '-graph').append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg.append("defs").append("clipPath")
-    .attr("id", "clip")
-  .append("rect")
-    .attr("width", width)
-    .attr("height", height);
+  var path = svg.append("g")
+      .attr("clip-path", "url(#clip)")
+    .append("path")
+      .datum(data[id])
+      .attr("class", "line-pos");
 
-svg.append("g")
-    .attr("class", "y axis")
-    .call(d3.svg.axis().scale(y).orient("left"));
+  svg.append("defs").append("clipPath")
+      .attr("id", "clip")
+    .append("rect")
+      .attr("width", width)
+      .attr("height", height);
 
-var xAxis = svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(x.axis = d3.svg.axis().scale(x).orient("bottom"));
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(d3.svg.axis().scale(y).orient("left"));
 
-var path = svg.append("g")
-    .attr("clip-path", "url(#clip)")
-  .append("path")
-    .datum(data)
-    .attr("class", "line-pos");
+  var xAxis = svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(x.axis = d3.svg.axis().scale(x).orient("bottom"));
 
-var transition = d3.select({}).transition()
-  .duration(750)
-  .ease("linear");
+  var transition = d3.select({}).transition()
+      .duration(750)
+      .ease("linear");
 
-tick();
-function tick() {
-  transition = transition.each(function() {
+  tick();
+  function tick() {
+    transition = transition.each(function() {
+      // update the domains
+      now = new Date();
+      x.domain([now - (n - 2) * duration, now - duration]);
 
-    // update the domains
-    now = new Date();
-    x.domain([now - (n - 2) * duration, now - duration]);
+      // push new data point onto the back
+      data[id].push(random());
 
-    // push new data point onto the back
-    data.push(random());
+      // redraw the line
+      path.attr("d", line)
+          .attr("transform", null);
 
-    // redraw the line
-    path.attr("d", line)
-        .attr("transform", null);
+      // slide the x-axis left
+      xAxis.call(x.axis);
 
-    // slide the x-axis left
-    xAxis.call(x.axis);
+      // slide the line left
+      path.transition()
+          .attr("transform", "translate(" + x(now - (n - 1) * duration) + ")");
 
-    // slide the line left
-    path.transition()
-        .attr("transform", "translate(" + x(now - (n - 1) * duration) + ")");
+      // pop the old data point off the front
+      data[id].shift();
+    }).transition().each("start", tick);
+  }
+}
 
-    // pop the old data point off the front
-    data.shift();
+data = {
+  'clinton': d3.range(n).map(random),
+  'trump': d3.range(n).map(random),
+  'sanders': d3.range(n).map(random),
+  'cruz': d3.range(n).map(random),
+}
 
-  }).transition().each("start", tick);
+for (id in data) {
+  graph(id);
 };
