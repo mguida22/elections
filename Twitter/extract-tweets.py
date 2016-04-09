fc961f6596cd7febe7c3e8e7b234a2abb4d80d51
narainsharma <narain280493@gmail.com>
2016-04-08 21:56:58 -0600

    Added storm support for tweets

--- Twitter/extract-tweets.py
+++ Twitter/extract-tweets.py
@@ -1,8 +1,11 @@
+# _*_ coding:utf-8 _*_
 import tweepy
 import sys
 import pymongo
 from httplib import IncompleteRead
 from Config_Utils.config import config
+from pykafka import KafkaClient
+
 
 CONSUMER_KEY = config.get_environment_variable('TWITTER_CONSUMER_KEY')
 CONSUMER_SECRET = config.get_environment_variable('TWITTER_CONSUMER_SECRET')
@@ -14,7 +17,10 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-
+''' A kafka queue is created with a "topic" and the tweets are written on to that topic.
+The storm topology is run and the spout reads off the tweets from the kafka queue
+ and passes it to the bolt where further processing of the tweet can be done.
+'''
 class TweetExtractor(tweepy.StreamListener):
 
     def __init__(self, api):
@@ -23,7 +29,7 @@ class TweetExtractor(tweepy.StreamListener):
         self.Tweets = pymongo.MongoClient().tweets
 
     def on_status(self, status):
-        # print status.text, "\n"
+        #print status.text, "\n"
 
         data = {}
 
@@ -31,7 +37,31 @@ class TweetExtractor(tweepy.StreamListener):
         data['text'] = status.text
         data['created_at'] = status.created_at
         data['geo'] = status.geo
-        self.save_tweets_to_db(data)
+       # print status.text
+       
+        #saving to MongoDB; comment out if not needed
+        #self.save_tweets_to_db(data)
+
+        #writing to Kafka Queue
+        client = KafkaClient(hosts='127.0.0.1:9092')
+        topic = client.topics[str('newtweets')]
+        with topic.get_producer(delivery_reports=False) as producer:
+            sentence = status.text.encode('ascii','ignore')
+            print sentence
+            producer.produce(sentence)
+            '''for word in sentence.split(" "):
+            if word is None:
+                continue
+            try:
+                word = str(word)
+                completesentence = completesentence + word + " "
+                #print word
+              #  producer.produce(word)
+            except:
+                continue
+        print completesentence
+        producer.produce(completesentence)
+        completesentence = ""'''
 
     def save_tweets_to_db(self, data):
         self.Tweets.tweets.insert(data)
