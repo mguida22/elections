32b757f2f262b9e3b5daa34f41439c5a779e186e
719Ben <ben.williams@colorado.edu>
2016-04-25 00:31:50 -0600

    Finised logic for agr. messages

--- go-kafka/main.go
+++ go-kafka/main.go
@@ -7,12 +7,12 @@ import (
 	"time"
 )
 
-type message struct {
+type feedMessage struct {
 	Sentiment string
 	Candidate string
 }
 
-type candidateSediment struct {
+type score struct {
 	positive int
 	negative int
 }
@@ -47,109 +47,92 @@ func main() {
 		}
 	}()
 
-	//create an array of messages, this is so that we can build up things
-	//before sending them to the client
-	setArray := make([]message, 0)
-
 	//create a message once so we can add to it later
-	newSentiment := message{}
+	newSentiment := feedMessage{}
+
+	//create a map for a new sentiment
+	candidateMap := make(map[string]*score)
+
+	//count for amount of messages managed
+	messagesRead := 0
 
 	//create a tick so that we know to send messages every two seconds
 	//this can be overridden if the array is too large
 	tick := time.Tick(time.Second * 2)
 
-	candidateSenders := []candidateSender{}
-
-	candidateSenders = append(candidateSenders,
-		candidateSender{"hillaryclinton", 0, 0},
-		candidateSender{"donaldtrump", 0, 0},
-		candidateSender{"berniesanders", 0, 0},
-		candidateSender{"tedcruz", 0, 0},
-		candidateSender{"johnkasich", 0, 0},
-		candidateSender{"marcorubio", 0, 0})
-
-	canNames := []string{}
-
 	for {
 		select {
+
+		//if there is a message
 		case msg := <-partitionConsumer.Messages():
 			{
+				//put the json values into the struct
 				json.Unmarshal(msg.Value, &newSentiment)
 
-				if len(setArray) > 50 {
+				//if the message is for no one
+				if newSentiment.Candidate == "none" {
+					break
+				}
+
+				positive := false
+
+				if newSentiment.Sentiment == "pos" {
+					positive = true
+				}
+
+				//if the candidate is not already in the map
+				if _, ok := candidateMap[newSentiment.Candidate]; !ok {
+
+					candidateMap[newSentiment.Candidate] = &score{}
+				}
+
+				//if the tweet is postive
+				if positive {
+					candidateMap[newSentiment.Candidate].positive++
+
+				} else {
+					candidateMap[newSentiment.Candidate].negative++
+				}
+
+				messagesRead++
+
+				//if there are more than 100 messages, go ahead and send it
+				if messagesRead > 100 {
 
 					//send the array to the client now
 					tick = time.Tick(time.Nanosecond)
 				}
 			}
+
+		//send messages out every two seconds
 		case <-tick:
 			{
+				if messagesRead != 0 {
+
+					//logic to send message to client goes here
+					//
+					fullArray := make(map[string]score)
+
+					for key, value := range candidateMap {
+
+						fullArray[key] = *value
+					}
+
+					log.Println(fullArray)
+					//
+					//end logic
+
+					log.Println(messagesRead, "messages read")
+					messagesRead = 0
+
+					for key := range candidateMap {
+
+						delete(candidateMap, key)
+					}
+				}
 
 				tick = time.Tick(time.Second * 2)
 			}
 		}
 	}
-}
-
-type senderMap []candidateSender
-
-func (sendMap *senderMap) addSentiment(sender message) {
-
-	if len(sendMap) == 5 {
-		log.Panic("Invalid senderMap, length should be 5")
-	}
-
-	switch sender.Candidate {
-	case "hillaryclinton":
-		{
-			if sender.Sentiment == "pos" {
-				sendMap[0].positive++
-			} else {
-				sendMap[0].negative++
-			}
-		}
-	case "donaldtrump":
-		{
-			if sender.Sentiment == "pos" {
-				sendMap[1].positive++
-			} else {
-				sendMap[1].negative++
-			}
-		}
-	case "berniesanders":
-		{
-			if sender.Sentiment == "pos" {
-				sendMap[2].positive++
-			} else {
-				sendMap[2].negative++
-
-			}
-		}
-	case "tedcruz":
-		{
-			if sender.Sentiment == "pos" {
-				sendMap[3].positive++
-			} else {
-				sendMap[3].negative++
-
-			}
-		}
-	case "johnkasich":
-		{
-			if sender.Sentiment == "pos" {
-				sendMap[4].positive++
-			} else {
-				sendMap[4].negative++
-			}
-		}
-	case "marcorubio":
-		{
-			if sender.Sentiment == "pos" {
-				sendMap[5].positive++
-			} else {
-				sendMap[5].negative++
-			}
-		}
-	}
-
 }
