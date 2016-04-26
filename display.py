dfc230c894229f2b7fc7ea915daf9731459ad395
719Ben <ben.williams@colorado.edu>
2016-04-25 22:15:49 -0600

    Made display.py work for any message topic

--- display.py
+++ display.py
@@ -1,9 +1,15 @@
 from pykafka import KafkaClient
 import json
+import sys
+
+if len(sys.argv) < 2:
+    print("please enter a command line argument for the stream you want to read")
+    print("example: python display.py sentimentfeed")
+    exit()
 
 
 client = KafkaClient(hosts='127.0.0.1:9092')
-sentiment_topic = client.topics[bytes('sentimentfeed', 'utf-8')]
+sentiment_topic = client.topics[bytes(sys.argv[1], 'utf-8')]
 sentiment_consumer = sentiment_topic.get_simple_consumer()
 
 for message in sentiment_consumer:
