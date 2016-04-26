962bfe4d996e085453851726a4e58f4299721576
mguida22 <michael.guida@colorado.edu>
2016-04-26 01:11:29 -0600

    connected real data feed to site
    built out groups for each candidate/sentiment

--- website/static/index.js
+++ website/static/index.js
@@ -1,15 +1,18 @@
-// Create a new HTML5 EventSource
+var d;
 var source = new EventSource('events/');
-// Create a callback for when a new message is received.
 source.onmessage = function(e) {
-  // Append the `data` attribute of the message to the DOM.
-  // document.body.innerHTML += e.data + '<br>';
-  console.log(e.data);
+  d = JSON.parse(e.data);
+  if (d) {
+    groups.sanders.pos.val = d.berniesanders.Positive * 100;
+    groups.sanders.neg.val = d.berniesanders.Negative * 100;
+    groups.trump.pos.val = d.donaldtrump.Positive * 100;
+    groups.trump.neg.val = d.donaldtrump.Negative * 100;
+    groups.clinton.pos.val = d.hillaryclinton.Positive * 100;
+    groups.clinton.neg.val = d.hillaryclinton.Negative * 100;
+    groups.cruz.pos.val = d.tedcruz.Positive * 100;
+    groups.cruz.neg.val = d.tedcruz.Negative * 100;
+  }
 };
-
-function random() {
-  return Math.floor(Math.random() * 101);
-}
 
 var n = 50,
   duration = 2000,
@@ -32,86 +35,121 @@ var line = d3.svg.line()
     .x(function(d, i) { return x(now - (n - 1 - i) * duration); })
     .y(function(d, i) { return y(d); });
 
-function graph(id, types) {
+function graph(id) {
   var svg = d3.select("#" + id + '-graph').append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  types.forEach(function(type) {
-    var path = svg.append("g")
-        .attr("clip-path", "url(#clip)")
-      .append("path")
-        .datum(data[id][type])
-        .attr("class", "line-" + type);
+  groups[id].xAxis = svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(x.axis = d3.svg.axis().scale(x).orient("bottom"));
 
-    svg.append("defs").append("clipPath")
-        .attr("id", "clip")
-      .append("rect")
-        .attr("width", width)
-        .attr("height", height);
+  groups[id].paths = svg.append('g');
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(d3.svg.axis().scale(y).orient("left"));
+  var yAxis = svg.append("g")
+      .attr("class", "y axis")
+      .call(d3.svg.axis().scale(y).orient("left"));
 
-    var xAxis = svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(x.axis = d3.svg.axis().scale(x).orient("bottom"));
+  ['pos', 'neg'].forEach(function(type) {
+    groups[id][type].path = groups[id].paths
+      .append('path')
+      .data([groups[id][type].data])
+      .attr("class", "line-" + type);
 
-    var transition = d3.select({}).transition()
-        .duration(duration)
-        .ease("linear");
+    // groups[id][type].path = svg.append('g')
+    //   .attr("clip-path", "url(#clip)")
+    //   .append('path')
+    //   .data([groups[id][type].data])
+    //   .attr("class", "line-" + type);
 
-    tick();
-    function tick() {
-      transition = transition.each(function() {
-        // update the domains
-        now = new Date();
-        x.domain([now - (n - 2) * duration, now - duration]);
-
-        // push new data point onto the back
-        data[id][type].push(random());
-
-        // redraw the line
-        path.attr("d", line)
-            .attr("transform", null);
-
-        // slide the x-axis left
-        xAxis.call(x.axis);
-
-        // slide the line left
-        path.transition()
-            .attr("transform", "translate(" + x(now - (n - 1) * duration) + ")");
-
-        // pop the old data point off the front
-        data[id][type].shift();
-      }).transition().each("start", tick);
-    }
+    // svg.append("defs").append("clipPath")
+    //   .attr("id", "clip")
+    // .append("rect")
+    //   .attr("width", width)
+    //   .attr("height", height);
   });
 }
 
-data = {
-  'clinton': {
-    'pos': d3.range(n).map(random),
-    'neg': d3.range(n).map(random),
+function tick() {
+  now = new Date();
+
+  for (id in groups) {
+    ['pos', 'neg'].forEach(function(type) {
+      groups[id][type].data.push(groups[id][type].val);
+      // groups[id][type].data.push(20 + Math.random() * 100);
+      groups[id][type].path.attr('d', line);
+    });
+  }
+
+  x.domain([now - (n - 2) * duration, now - duration]);
+
+  for (id in groups) {
+    groups[id].xAxis.transition()
+      .duration(duration)
+      .ease('linear')
+      .call(x.axis);
+
+    groups[id].paths.attr('transform', null)
+      .transition()
+      .duration(duration)
+      .ease('linear')
+      .attr('transform', 'translate(' + x(now - (n - 1) * duration) + ')')
+      .each('end', tick);
+
+    ['pos', 'neg'].forEach(function(type) {
+      groups[id][type].data.shift();
+    });
+  }
+}
+
+var groups = {
+  clinton: {
+    pos: {
+      data: d3.range(n).map(function() { return 0; }),
+      val: 0,
+    },
+    neg: {
+      data: d3.range(n).map(function() { return 0; }),
+      val: 0,
+    }
   },
-  'trump': {
-    'pos': d3.range(n).map(random),
-    'neg': d3.range(n).map(random),
+  trump: {
+    pos: {
+      data: d3.range(n).map(function() { return 0; }),
+      val: 0,
+    },
+    neg: {
+      data: d3.range(n).map(function() { return 0; }),
+      val: 0,
+    }
   },
-  'sanders': {
-    'pos': d3.range(n).map(random),
-    'neg': d3.range(n).map(random),
+  sanders: {
+    pos: {
+      data: d3.range(n).map(function() { return 0; }),
+      val: 0,
+    },
+    neg: {
+      data: d3.range(n).map(function() { return 0; }),
+      val: 0,
+    }
   },
-  'cruz': {
-    'pos': d3.range(n).map(random),
-    'neg': d3.range(n).map(random),
+  cruz: {
+    pos: {
+      data: d3.range(n).map(function() { return 0; }),
+      val: 0,
+    },
+    neg: {
+      data: d3.range(n).map(function() { return 0; }),
+      val: 0,
+    }
   },
 };
 
-for (id in data) {
-  graph(id, ['pos', 'neg']);
+for (id in groups) {
+  graph(id);
 };
+
+tick();
